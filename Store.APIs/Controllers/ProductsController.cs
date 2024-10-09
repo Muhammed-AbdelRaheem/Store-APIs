12e26880f80f3eee93266324077b7c3687d3b614
Muhammed-AbdelRaheem <MuhammedAbdelRaheem93@gmail.com>
2024-10-09 15:53:25 +0300

    Improving Swagger Documentation

--- Store.APIs/Controllers/ProductsController.cs
+++ Store.APIs/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Store.APIs.Errors;
 using Store.Core.Dtos;
 using Store.Core.Helper;
 using Store.Core.Servecies.Contract;
@@ -19,43 +20,57 @@ namespace Store.APIs.Controllers
 
 
 
+
+        [ProducesResponseType(typeof(PaginationResponse<ProductDto>),StatusCodes.Status200OK)]
         [HttpGet]
-        public async Task<IActionResult> GetAllProducts([FromQuery] ProductSpecParams productSpec)
+        public async Task<ActionResult<PaginationResponse<ProductDto>>> GetAllProducts([FromQuery] ProductSpecParams productSpec)
         {
             var result = await _productService.GetAllProductsAsync(productSpec);
             return Ok(result);
         }
 
 
+
+        [ProducesResponseType(typeof(IEnumerable<TypeBrandDto>), StatusCodes.Status200OK)]
         [HttpGet("Brands")]
-        public async Task<IActionResult> GetAllBrands()
+        public async Task<ActionResult<IEnumerable<TypeBrandDto>>> GetAllBrands()
         {
             var result = await _productService.GetAllBrandsAsync();
 
             return Ok(result);
         }
 
+
+        [ProducesResponseType(typeof(IEnumerable<TypeBrandDto>), StatusCodes.Status200OK)]
         [HttpGet("Types")]
-        public async Task<IActionResult> GetAllTypes()
+        public async Task<ActionResult<IEnumerable<TypeBrandDto>>> GetAllTypes()
         {
             var result = await _productService.GetAllTypesAsync();
 
             return Ok(result);
         }
 
+
+
+        [ProducesResponseType(typeof(TypeBrandDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiErrorResponse), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ApiErrorResponse), StatusCodes.Status404NotFound)]
+
+
+
         [HttpGet("{id}")]
         public async Task<IActionResult> GetProductById(int? id)
         {
             if (id is null)
             {
-                BadRequest("Invalid Id !!");
+                BadRequest(new ApiErrorResponse(400));
             }
             var result = await _productService.GetProductById(id.Value);
 
             if (result is null)
             {
                 return
-                NotFound($"The Product With ID : {id} Not Found In Db");
+                NotFound(new ApiErrorResponse(404));
             }
 
             return Ok(result);
