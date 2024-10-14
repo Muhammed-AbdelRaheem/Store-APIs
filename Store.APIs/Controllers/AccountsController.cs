2b8813a39986d39aadb0ff422b9206b10fec3c1a
Muhammed-AbdelRaheem <MuhammedAbdelRaheem93@gmail.com>
2024-10-14 03:16:18 +0300

    Get Current User

--- Store.APIs/Controllers/AccountsController.cs
+++ Store.APIs/Controllers/AccountsController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Store.APIs.Errors;
 using Store.Core.Dtos.Auth;
+using Store.Core.Entities.Identity;
 using Store.Core.Servecies.Contract;
+using System.Security.Claims;
 
 namespace Store.APIs.Controllers
 {
@@ -11,10 +13,16 @@ namespace Store.APIs.Controllers
     public class AccountsController : BaseApiController
     {
         private readonly IUserService _userService;
+        private readonly ITokenService _tokenService;
+        private readonly UserManager<AppUser> _userManager;
 
-        public AccountsController(IUserService userService)
+        public AccountsController(IUserService userService,
+                                  UserManager<AppUser> userManager,
+                                  ITokenService tokenService)
         {
             _userService = userService;
+            _tokenService = tokenService;
+            _userManager = userManager;
         }
 
 
@@ -41,6 +49,29 @@ namespace Store.APIs.Controllers
 
             return Ok(user);
         }
+
+
+
+        [HttpGet("GetCurrentUser")] // Get : /Api/Accounts/GetCurrentUser
+
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            var userEmail = User.FindFirstValue(ClaimTypes.Email);
+            if (userEmail is null) return BadRequest(new ApiErrorResponse(StatusCodes.Status400BadRequest));
+
+            var user =await _userManager.FindByEmailAsync(userEmail);
+            if (user is null) return BadRequest(new ApiErrorResponse(StatusCodes.Status400BadRequest));
+
+            return Ok(new UserDto()
+            {
+                DisplayName = user.DisplayName,
+                Email = user.Email,
+                Token = await _tokenService.CreateTokenAsync(user, _userManager)
+
+
+            });
+        }
+
 
 
     }
